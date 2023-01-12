10b30f4eff3771135bba0a66d04dc9d0f3021eb9
Vitor Hugo Padovan Hernandes <vitor.padovan89@gmail.com>
2023-01-12 16:50:40 -0300

    #12 Adicionando smart tags

---
+++ src/main/java/com/br/pcsemdor/portfolio/model/SmartTags.java
@@ -0,0 +1,24 @@
+package com.br.pcsemdor.portfolio.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = { "name" }, name = "uk_name"))
+public class SmartTags {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idSmartTag;
+
+	@Column(nullable = false)
+	private String name;
+}
