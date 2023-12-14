47548bcc1cd7f66df7d487613ee8a22314cb9f05
Vitor Hugo Padovan Hernandes <vitor.padovan89@gmail.com>
2023-12-14 09:06:40 -0300

    Adicionando configurações para dev

--- src/main/java/com/br/pcsemdor/portfolio/config/security/ServerConfig.java
+++ src/main/java/com/br/pcsemdor/portfolio/config/security/ServerConfig.java
@@ -4,38 +4,47 @@ import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+
+import lombok.extern.log4j.Log4j2;
 
 @Configuration
+@Log4j2
 public class ServerConfig {
-
+	
+	
 	@Bean
+	@Profile("production")
 	public ServletWebServerFactory servletContainer() {
+		
 		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-
 			@Override
-			protected void postProcessContext(Context context) {
-//				var securityConstraint = new SecurityConstraint();
-//				securityConstraint.setUserConstraint("CONFIDENTIAL");
-//				var collection = new SecurityCollection();
-//				collection.addPattern("/*");
-//				securityConstraint.addCollection(collection);
-//				context.addConstraint(securityConstraint);
+			protected void postProcessContext(Context context) {				
+				var securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				var collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
 			}
 		};
 		tomcat.addAdditionalTomcatConnectors(getHttpConnector());
 		return tomcat;
 	}
 
+	@Profile("production")
 	private Connector getHttpConnector() {
 		var connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
 		connector.setScheme("http");
 		connector.setPort(80);
 		connector.setSecure(false);
-//		connector.setRedirectPort(443);
+		connector.setRedirectPort(443);
 		return connector;
 	}
 }
